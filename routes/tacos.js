0fd49aa57eb85c7d8ea54444c7b8b17868608988
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-11 13:51:15 -0700

    basic connection to SQL db

---
+++ routes/tacos.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var conString = "postgres://@localhost/taco_types";
+
+router.get('/', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT * from tacos', function(err, result) {
+      done();
+      res.render('tacos/index', {tacos: result.rows})
+      if (err) {
+        return console.error('error running query', err);
+      }
+      console.log(result.rows[0].number);
+      console.log("connected to database")
+    });
+
+  });
+});
+
+module.exports = router;
